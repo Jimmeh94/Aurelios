2dbac004d4962dd665a8fc54e038353f128c81f7
Jimmeh <jimmy.walters94@yahoo.com>
2018-03-23 01:22:08 -0500

    Remove duplicate code

--- src/main/java/com/aurelios/server/game/user/ai/tasks/TaskWork.java
+++
@@ -1,8 +0,0 @@
-package com.aurelios.server.game.user.ai.tasks;
-
-import com.aurelios.server.game.user.ai.DailySchedule;
-
-public class TaskWork extends Task {
-
-    public TaskWork(int howLongToAccomplish, DailySchedule owner) {
-        super(howLongToAccomplish, owner, TaskType.WORK
--- src/main/java/com/aurelios/server/game/user/modules/UserModuleTypes.java
+++
@@ -1,5 +0,0 @@
-package com.aurelios.server.game.user.modules;
-
-public enum UserModuleTypes {
-
-    NODE_CREATOR, POI_CREATOR, POLL_BUILDER
